e55be1c167dad0a9d0fcf0fb7375ff00c7b35322
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 22:09:55 +0300

    Add project files.

---
+++ odev_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int toplam = 0;
+            
+            while (sayi > 0)
+            {
+                toplam += sayi % 10;
+                sayi /= 10;
+            }
+
+            Console.WriteLine("Sonuç : " + toplam);
+            Console.ReadKey();
+        }
+
+    }
+}
